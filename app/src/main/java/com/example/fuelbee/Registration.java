57b4b1621bdfa53eb51ed9f09e4842443811b4e1
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-06-26 23:55:33 +0600

    Login Signup page

--- app/src/main/java/com/example/fuelbee/Registration.java
+++ app/src/main/java/com/example/fuelbee/Registration.java
@@ -3,12 +3,43 @@ package com.example.fuelbee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Registration extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Registration.this, "Enter email", Toast.LENGTH_SHORT).show();
+                return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
+                return;
+                }
+            }
+        });
     }
 }
