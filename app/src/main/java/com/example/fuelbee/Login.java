7e09f2a1411a747a56fb53707d0fc206684f9f7f
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-06-29 02:24:42 +0600

    Forgot pass integrated

--- app/src/main/java/com/example/fuelbee/Login.java
+++ app/src/main/java/com/example/fuelbee/Login.java
@@ -1,7 +1,9 @@
 package com.example.fuelbee;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +12,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,6 +29,8 @@ public class Login extends AppCompatActivity {
     private TextView signupRedirect;
 
     private Button loginButton;
+
+    TextView forgotPassword;
 
 
     @Override
@@ -36,6 +43,7 @@ public class Login extends AppCompatActivity {
         loginPass = findViewById(R.id.password);
         loginButton = findViewById(R.id.btn_login);
         signupRedirect = findViewById(R.id.noAccount);
+        forgotPassword = findViewById(R.id.forgot_password);
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -78,6 +86,54 @@ public class Login extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         startActivity(new Intent(Login.this, Registration.class));
+                    }
+                });
+
+                forgotPassword.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                        View dialogView = getLayoutInflater().inflate(R.layout.dialog_forget, null);
+                        EditText emailBox = dialogView.findViewById(R.id.emailBox);
+
+                        builder.setView(dialogView);
+                        AlertDialog dialog = builder.create();
+
+                        dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String userEmail = emailBox.getText().toString();
+                                if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                                    Toast.makeText(Login.this, "Enter your registered Email Id", Toast.LENGTH_SHORT).show();
+                                return;
+                                }
+
+                                auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(Login.this, "Password reset link send to your email", Toast.LENGTH_SHORT).show();
+                                        }else{
+                                            Toast.makeText(Login.this, "Unable to send the password reset link", Toast.LENGTH_SHORT).show();
+                                        }
+                                        dialog.dismiss();
+                                    }
+                                });
+                            }
+                        });
+
+                        dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        if (dialog.getWindow() != null){
+                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                        }
+                        dialog.show();
+
                     }
                 });
     }
